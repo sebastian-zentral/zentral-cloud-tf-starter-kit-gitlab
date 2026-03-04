8d42a4e37ecbf2ff7a8b3ec950a1abe65a507300
Éric Falconnier <eric@zentral.com>
2026-03-04 13:21:37 +0100

    Better Default MDM blueprint config
     - FileVault
     - Recovery Password
     - DDM Software Updates

--- mdm_default_blueprint.tf
+++ mdm_default_blueprint.tf
@@ -1,8 +1,39 @@
 resource "zentral_mdm_blueprint" "default" {
-  name                 = "Default"
-  collect_apps         = "ALL"
-  collect_certificates = "ALL"
-  collect_profiles     = "ALL"
+  name                            = "Default"
+  collect_apps                    = "ALL"
+  collect_certificates            = "ALL"
+  collect_profiles                = "ALL"
+  legacy_profiles_via_ddm         = true
+  filevault_config_id             = zentral_mdm_filevault_config.default.id
+  recovery_password_config_id     = zentral_mdm_recovery_password_config.default.id
+  software_update_enforcement_ids = [zentral_mdm_software_update_enforcement.default.id]
+}
+
+# FileVault
+
+resource "zentral_mdm_filevault_config" "default" {
+  name                         = "Default"
+  escrow_location_display_name = "Zentral Cloud"
+  at_login_only                = true
+  bypass_attempts              = 0
+  destroy_key_on_standby       = true
+  show_recovery_key            = false
+}
+
+# Recovery password
+
+resource "zentral_mdm_recovery_password_config" "default" {
+  name = "Default"
+}
+
+# Software update enforcement
+
+resource "zentral_mdm_software_update_enforcement" "default" {
+  name           = "Default"
+  platforms      = ["macOS"]
+  max_os_version = "27"
+  delay_days     = 7
+  details_url    = "https://support.apple.com/en-us/122868"
 }
 
 # Bootstrap package
